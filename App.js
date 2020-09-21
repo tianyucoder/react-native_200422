baea6471abe7aa058c9b557dbe4b33ee043cd0b0
tianyu5177 <zhangtianyu5177@sina.com>
2020-09-21 10:49:57 +0800

    'init'

---
+++ App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import {Text} from 'react-native'
+
+export default class App extends Component {
+	render() {
+		return (
+			<Text style={{fontSize:40}}>hello,我要开始学习RN了，虽然还需要磨合</Text>
+		)
+	}
+}
