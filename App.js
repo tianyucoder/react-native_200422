41c71c924f07036e69a17d90271d3ea59820ad70
tianyu5177 <zhangtianyu5177@sina.com>
2020-09-21 12:01:35 +0800

    更新

--- App.js
+++ App.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react'
-import {Text} from 'react-native'
+import {Text,StatusBar,View,StyleSheet} from 'react-native'
 
 export default class App extends Component {
 	render() {
 		return (
-			<Text style={{fontSize:40}}>hello,我要开始学习RN了，虽然还需要磨合</Text>
+			<>
+				<StatusBar
+					backgroundColor="orange"
+					barStyle="dark-content"
+					// hidden={true}
+					// animated={true}
+					// translucent={true}
+				/>
+				<View style={style.top}>
+					<Text style={style.topText}>hello,我要开始学习RN了</Text>
+				</View>
+				<View style={style.footer}>
+					<Text>hello,我要开始学习RN了</Text>
+				</View>
+				
+			</>
 		)
 	}
 }
+
+const style = StyleSheet.create({
+	top:{
+		// fontSize:30,
+		backgroundColor:'skyblue'
+	},
+	topText:{
+		fontSize:30
+	},
+	footer:{
+		// fontSize:20,
+		backgroundColor:'orange'
+	}
+})
